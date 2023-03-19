0f152b0e4142b274a352ee7aacf1380003fb99b9
GHSankalpa <106649969+GHSankalpa@users.noreply.github.com>
2023-03-19 21:11:09 +0530

    Update teacher-table.component.ts

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -61,18 +61,17 @@ export class TeacherTableComponent implements OnInit {
   }
 
   search(value) {
-    let foundItems = [];
-    if (value.length <= 0) {
-      this.getTeacherData();
-    } else {
-      let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
-          foundItems.push(teacher)
-        }
-      });
-      this.teacherData = foundItems;
-    }
+  if (value.length <= 0) {
+    this.getTeacherData();
+  } else {
+    this.service.searchTeacher(value).subscribe((response) => {
+      this.teacherData = Object.keys(response).map((key) => [response[key]]);
+    }, (error) => {
+      console.log('ERROR - ', error)
+    });
   }
+}
+
 
   deleteTeacher(itemid) {
     const test = {
